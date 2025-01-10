e7c647462283a13b8ed00fec0a5162f167860176
Ehsan <ehsan_ai@proton.me>
2025-01-10 19:22:21 +0330

    Created GameBorad app

--- LustigesDeutsch/views.py
+++ LustigesDeutsch/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 # Create your views here
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def playervsplayer(request, gameName = 'vokabel'):
+    request.session.flush()
+    if request.method == 'POST':
+        request.session['radioOption'] = request.POST['radioOption']
+        return redirect(reverse(f'{gameName}:playervsplayer'))
+        # print(request.session['radioOption'])
+    return render(request, 'playervsplayer.html')
+
